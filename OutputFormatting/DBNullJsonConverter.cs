898d6756ddae381318cfa8e7a4aca25180b13171
Nick.Harvey <Nick.Harvey@veteransunited.com>
2024-01-13 11:07:41 -0600

    handle DBNull in JSON output mode

---
+++ OutputFormatting/DBNullJsonConverter.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace QueryTerminal.OutputFormatting;
+
+class DBNullJsonConverter : JsonConverter<DBNull>
+{
+    public override DBNull Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotSupportedException("DBNull values are not expected in input JSON data");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DBNull value, JsonSerializerOptions options)
+    {
+        writer.WriteNullValue();
+    }
+
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert == typeof(DBNull);
+    }
+}
+
