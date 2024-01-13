77ba539483a3f04dbf415e0e36758ad214e4a788
Nick.Harvey <Nick.Harvey@veteransunited.com>
2024-01-12 21:54:33 -0600

    use spectre to create more pretty json output

--- OutputFormatting/JsonOutputFormatter.cs
+++ OutputFormatting/JsonOutputFormatter.cs
@@ -1,14 +1,18 @@
 using System.Data;
 using System.Text.Json;
+using Spectre.Console;
+using Spectre.Console.Json;
 
 namespace QueryTerminal.OutputFormatting;
 
 public class JsonOutputFormatter : IOutputFormatter
 {
+    private readonly bool _pretty;
     private readonly JsonSerializerOptions _jsonSerializerOptions;
 
     public JsonOutputFormatter(bool pretty = false)
     {
+        _pretty = pretty;
         _jsonSerializerOptions = new JsonSerializerOptions {
             WriteIndented = pretty,
             TypeInfoResolver = SourceGenerationContext.Default
@@ -25,6 +29,14 @@ public class JsonOutputFormatter : IOutputFormatter
             values.Add(Enumerable.Range(0, count).Select(i => new KeyValuePair<string,object>(reader.GetName(i), row[i])).ToDictionary<string,object>());
         }
         var json = JsonSerializer.Serialize(values, typeof(List<Dictionary<string,object>>), _jsonSerializerOptions);
-        Console.WriteLine(json);
+        if (_pretty)
+        {
+            AnsiConsole.Write(new JsonText(json));
+            AnsiConsole.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine(json);
+        }
     }
 }
